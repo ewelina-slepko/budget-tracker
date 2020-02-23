f0c4aff7cd4b5656343fe42f19727b4bfdfabb02
ewelina.slepko <slepko.ewelina@gmail.com>
2020-02-23 22:08:27 +0100

    Code refactoring.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -5,31 +5,24 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
-
-  active = 1;
+export class LandingPageComponent {
 
   firstPage = 1;
   secondPage = 2;
   lastPage = 3;
 
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+  active = this.firstPage;
 
   toggleActiveOnClick(param: number) {
     this.active = param;
-    this.active = param;
-  }
-
-  toggleActiveOnSwipeLeft() {
-    return this.active < this.lastPage ? this.active++ : this.active = 1;
   }
 
   toggleActiveOnSwipeRight() {
-    return this.active > this.firstPage ? this.active-- : this.active = 3;
+    return this.active < this.lastPage ? this.active++ : this.active = this.firstPage;
+  }
+
+  toggleActiveOnSwipeLeft() {
+    return this.active > this.firstPage ? this.active-- : this.active = this.lastPage;
   }
 
 }
