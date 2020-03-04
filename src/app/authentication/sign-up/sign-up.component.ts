23c8804acd21b9e586b7ed916ee5743e84a116a5
ewelina.slepko <slepko.ewelina@gmail.com>
2020-03-04 22:31:17 +0100

    Changed validation strategy (using ControlValueAccessor and creating custom input)

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthenticationService} from '../../shared/services/authentication.service';
-import {isEmailValid, isNameValid, isPasswordValid} from '../utilities';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -8,19 +7,13 @@ import {NgForm} from '@angular/forms';
   templateUrl: './sign-up.component.html',
   styleUrls: ['../authentication.component.scss', './sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
-
-  isNameValid = isNameValid;
-  isEmailValid = isEmailValid;
-  isPasswordValid = isPasswordValid;
+export class SignUpComponent {
 
   constructor(public authService: AuthenticationService) {
   }
 
-  ngOnInit() {
-  }
-
   signUpWithEmailAndPassword(form: NgForm) {
+    console.log(form);
     this.authService.signUpWithEmailAndPassword(form.form.value.Email, form.form.value.Password);
   }
 }
--- src/app/shared/components/text-input/text-input.component.ts
+++ src/app/shared/components/text-input/text-input.component.ts
@@ -1,21 +1,51 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {ControlContainer, NgForm} from '@angular/forms';
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss'],
-  viewProviders: [{provide: ControlContainer, useExisting: NgForm}]
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextInputComponent),
+      multi: true
+    }
+  ],
 })
-export class TextInputComponent {
+export class TextInputComponent implements ControlValueAccessor {
+
 
   @Input() name: string;
   @Input() type: string;
-  @Input() field: string;
+  @Input() status: string;
 
-  @Input() isInputValid: boolean;
-
+  val = '';
   passwordIsVisible: boolean;
+
+  onChange: any = () => {
+  };
+  onTouch: any = () => {
+  };
+
+  set value(val) {
+    this.val = val;
+    this.onChange(val);
+    this.onTouch(val);
+  }
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any) {
+    console.log(this);
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouch = fn;
+  }
 
   togglePasswordVisibility() {
     this.passwordIsVisible = !this.passwordIsVisible;
