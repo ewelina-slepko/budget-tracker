10339f27e33cf64c8dcd310e4b75f529401f8297
ewelina.slepko <slepko.ewelina@gmail.com>
2020-03-21 19:30:35 +0100

    Income component - new features

--- src/app/home/layouts/income-form/income-form.component.ts
+++ src/app/home/layouts/income-form/income-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {daysAnimation} from './days-animation';
 
 @Component({
@@ -7,12 +7,15 @@ import {daysAnimation} from './days-animation';
   styleUrls: ['./income-form.component.scss'],
   animations: daysAnimation
 })
-export class IncomeFormComponent implements OnInit, OnChanges {
+export class IncomeFormComponent implements OnInit {
+
+  incomeNumber = 1;
+  incomes = [];
 
   daysOfMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   lastDayOfMonth = this.daysOfMonth[this.daysOfMonth.length - 1];
 
-  active: number;
+  activeDay: number;
   previous: number;
   next: number;
 
@@ -20,20 +23,16 @@ export class IncomeFormComponent implements OnInit, OnChanges {
   isSwipingDown = true;
 
   ngOnInit(): void {
-    this.active = 10;
+    this.incomes.push(this.incomeNumber);
+    this.activeDay = 10;
     this.setPreviousAndNextValue();
-  }
-
-  ngOnChanges() {
-    this.previous = this.active - 1;
-    this.next = this.active + 1;
   }
 
   toggleActiveOnSwipeUp() {
     this.isSwipingUp = true;
     this.isSwipingDown = false;
 
-    this.active = this.active < this.lastDayOfMonth ? this.active + 1 : this.active = 1;
+    this.activeDay = this.activeDay < this.lastDayOfMonth ? this.activeDay + 1 : this.activeDay = 1;
     this.setPreviousAndNextValue();
   }
 
@@ -41,13 +40,21 @@ export class IncomeFormComponent implements OnInit, OnChanges {
     this.isSwipingDown = true;
     this.isSwipingUp = false;
 
-    this.active = this.active > 1 ? this.active - 1 : this.lastDayOfMonth;
+    this.activeDay = this.activeDay > 1 ? this.activeDay - 1 : this.lastDayOfMonth;
     this.setPreviousAndNextValue();
   }
 
   setPreviousAndNextValue() {
-    this.previous = this.active !== 1 ? this.active - 1 : this.lastDayOfMonth;
-    this.next = this.active !== this.lastDayOfMonth ? this.active + 1 : 1;
+    this.previous = this.activeDay !== 1 ? this.activeDay - 1 : this.lastDayOfMonth;
+    this.next = this.activeDay !== this.lastDayOfMonth ? this.activeDay + 1 : 1;
+  }
+
+  addIncome() {
+    this.incomes.push(++this.incomeNumber);
+  }
+
+  saveIncomes(form) {
+    console.log(form.value, this.activeDay);
   }
 
 }
