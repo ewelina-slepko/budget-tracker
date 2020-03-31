8adca0727ae29805eae81febf93509c60d8eb836
ewelina.slepko <slepko.ewelina@gmail.com>
2020-03-31 22:30:11 +0200

    Added routing navigation - income component.

--- src/app/home/layouts/income-form/income-form.component.ts
+++ src/app/home/layouts/income-form/income-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {daysAnimation} from './days-animation';
 import {InitialSettingsService} from '../../cards/initial-settings/initial-settings.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'income-form',
@@ -22,7 +23,8 @@ export class IncomeFormComponent implements OnInit {
   isSwipingUp = false;
   isSwipingDown = true;
 
-  constructor(private initialSettingsService: InitialSettingsService) {
+  constructor(private initialSettingsService: InitialSettingsService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -62,5 +64,6 @@ export class IncomeFormComponent implements OnInit {
 
   saveIncomes(form) {
     console.log(form, this.activeDay);
+    this.router.navigate(['/user/initialsettings/step3']);
   }
 }
