857b51bd8f5cae8496d0ab4b50e2a0813a2b30aa
ewelina.slepko <slepko.ewelina@gmail.com>
2020-04-01 22:48:42 +0200

    Added checkbox to Budget component

--- src/app/shared/animation.ts
+++ src/app/shared/animation.ts
@@ -4,7 +4,7 @@ export const basicAnimation = [
   trigger('fade', [
     transition('void => *', [
       style({opacity: 0}),
-      animate(300, style({opacity: 1}))
+      animate(200, style({opacity: 1}))
     ])
   ])
 ];
