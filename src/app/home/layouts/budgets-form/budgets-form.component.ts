7d2df4deff7313a98abaf82434a7e3dd82cad7d7
ewelina.slepko <slepko.ewelina@gmail.com>
2020-04-02 23:39:43 +0200

    Added possibility to select cycle and category.

--- src/app/home/layouts/budgets-form/budgets-form.component.ts
+++ src/app/home/layouts/budgets-form/budgets-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {InitialSettingsService} from '../../cards/initial-settings/initial-settings.service';
 import {basicAnimation} from '../../../shared/animation';
+import {NgForm} from '@angular/forms';
+import {Categories, CategoryDto, CyclesDict, CyclesDto} from './dtos';
 
 @Component({
   selector: 'budgets-form',
@@ -11,7 +13,12 @@ import {basicAnimation} from '../../../shared/animation';
 export class BudgetsFormComponent implements OnInit {
 
   isNewBudgetCardVisible = false;
-  categories = ['Rent', 'Food', 'Netflix', 'Gym', 'Events', 'Pet', 'Charity'];
+
+  categories: CategoryDto[] = Categories;
+  cycles: CyclesDto[] = CyclesDict;
+
+  selectedCycle: string;
+  selectedCategory: string;
 
   constructor(private initialSettingsService: InitialSettingsService) {
   }
@@ -24,11 +31,26 @@ export class BudgetsFormComponent implements OnInit {
     this.initialSettingsService.sendCurrentStepInfo(3);
   }
 
+  selectCycle(cycle: CyclesDto) {
+    this.cycles.map(element => element.isSelected = false);
+    this.selectedCycle = cycle.name;
+    cycle.isSelected = true;
+  }
+
+  selectCategory(category: CategoryDto) {
+    this.categories.map(element => element.isSelected = false);
+    this.selectedCategory = category.name;
+    category.isSelected = true;
+  }
+
   addBudget() {
     this.isNewBudgetCardVisible = true;
   }
 
-  saveBudget() {
+  saveBudget(form: NgForm) {
+    if (form.form.status === 'VALID') {
+      console.log('form >>', form.form.value, 'cycle >>', this.selectedCycle, 'category', this.selectedCategory);
+    }
     this.isNewBudgetCardVisible = false;
   }
 }
---
+++ src/app/home/layouts/budgets-form/dtos.ts
@@ -0,0 +1,56 @@
+export const Categories = [
+  {
+    name: 'Rent',
+    isSelected: false
+  },
+  {
+    name: 'Food',
+    isSelected: false
+  },
+  {
+    name: 'Netflix',
+    isSelected: false
+  },
+  {
+    name: 'Gym',
+    isSelected: false
+  },
+  {
+    name: 'Events',
+    isSelected: false
+  },
+  {
+    name: 'Pet',
+    isSelected: false
+  },
+  {
+    name: 'Charity',
+    isSelected: false
+  }
+];
+
+export interface CategoryDto {
+  name: string;
+  isSelected: boolean;
+}
+
+
+export const CyclesDict = [
+  {
+    name: 'Weekly',
+    isSelected: false
+  },
+  {
+    name: 'Monthly',
+    isSelected: false
+  },
+  {
+    name: 'Yearly',
+    isSelected: false
+  }
+];
+
+export interface CyclesDto {
+  name: string;
+  isSelected: boolean;
+}
