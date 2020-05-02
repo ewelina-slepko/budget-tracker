757864fc39bdcec1785538a2f3839d54717ef2ab
ewelina.slepko <slepko.ewelina@gmail.com>
2020-05-02 13:24:03 +0200

    Added icons to categories

--- src/app/home/shared/forms/budgets-form/dtos.ts
+++ src/app/home/shared/forms/budgets-form/dtos.ts
@@ -11,36 +11,44 @@ export interface BudgetDto {
 export const categories = [
   {
     name: 'Rent',
-    isSelected: false
+    icon: 'fas fa-house-user',
+    isSelected: false,
   },
   {
     name: 'Food',
+    icon: 'fas fa-hamburger',
     isSelected: false
   },
   {
     name: 'Netflix',
+    icon: 'fas fa-tv',
     isSelected: false
   },
   {
     name: 'Gym',
+    icon: 'fas fa-dumbbell',
     isSelected: false
   },
   {
     name: 'Events',
+    icon: 'fas fa-birthday-cake',
     isSelected: false
   },
   {
     name: 'Pet',
+    icon: 'fas fa-fish',
     isSelected: false
   },
   {
     name: 'Charity',
+    icon: 'fas fa-hand-holding-heart',
     isSelected: false
   }
 ];
 
 export interface CategoryDto {
   name: string;
+  icon: string;
   isSelected: boolean;
 }
 
