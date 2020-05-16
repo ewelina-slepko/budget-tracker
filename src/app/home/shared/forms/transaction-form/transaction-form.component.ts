66a799caae0afd40c0a35f21cc703e548b1ca1eb
ewelina.slepko <slepko.ewelina@gmail.com>
2020-05-16 16:50:50 +0200

    Added animation to transaction-form

--- src/app/home/shared/forms/transaction-form/transaction-form.component.ts
+++ src/app/home/shared/forms/transaction-form/transaction-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {basicAnimation} from '../../../../shared/animation';
 import {PanelService} from '../../../panel/panel.service';
 
