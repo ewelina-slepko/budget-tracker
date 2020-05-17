99409651baceebd154ea8b8c441f8ee7fa6b76d9
ewelina.slepko <slepko.ewelina@gmail.com>
2020-05-17 20:44:15 +0200

    Added content to TransactionForm Component

--- src/app/home/shared/forms/transaction-form/transaction-form.component.ts
+++ src/app/home/shared/forms/transaction-form/transaction-form.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {basicAnimation} from '../../../../shared/animations/basic-animation';
 import {PanelService} from '../../../panel/panel.service';
+import {NgForm} from '@angular/forms';
+import {ApiService} from '../../../../shared/services/api.service';
+import {categories, BudgetDto, CategoryDto} from '../budgets-form/dtos';
 
 @Component({
   selector: 'transaction-form',
@@ -8,12 +11,42 @@ import {PanelService} from '../../../panel/panel.service';
   styleUrls: ['./transaction-form.component.scss'],
   animations: basicAnimation
 })
-export class TransactionFormComponent {
+export class TransactionFormComponent implements OnInit {
 
-  constructor(private panelService: PanelService) {
+  budgetsList: BudgetDto[];
+  categories: CategoryDto[] = categories;
+
+  constructor(private panelService: PanelService,
+              private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.getBudgetsList();
+  }
+
+  getBudgetsList() {
+    this.apiService.getBudgetsList().subscribe(res => {
+      this.budgetsList = res.map(budget => {
+        const budgetData = budget.payload.doc.data();
+        const id = budget.payload.doc.id;
+        return {id, ...budgetData};
+      });
+    });
   }
 
   closeNewTransactionForm() {
     this.panelService.sendNewTransactionFormStatus(false);
+  }
+
+  saveTransaction(form: NgForm) {
+    console.log(console.log('TODO', form));
+  }
+
+  checkIfStandardCategory(budget) {
+    return categories.map(category => category.name === budget.category && category.icon).filter(icon => icon).join();
+  }
+
+  checkIfCustomCategory(budget) {
+    return !categories.map(category => category.name).includes(budget.category);
   }
 }
