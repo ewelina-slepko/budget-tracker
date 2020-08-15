ae2c6013df0649176bc01abb8ff877cb2aed4c5f
ewelina.slepko <slepko.ewelina@gmail.com>
2020-08-15 15:05:12 +0200

    Added filter label component

---
+++ src/app/home/panel/cards/transactions/filter-label/filter-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-label',
+  templateUrl: './filter-label.component.html',
+  styleUrls: ['./filter-label.component.scss']
+})
+export class FilterLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
