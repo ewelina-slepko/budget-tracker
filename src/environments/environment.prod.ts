65f2f13c08312af8f29a560a0134c06c0db09276
ewelina.slepko <slepko.ewelina@gmail.com>
2020-08-27 23:26:50 +0200

    Fixed authentication bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    databaseURL: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    projectId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    messagingSenderId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    apiKey: 'AIzaSyDBHlFOFR3Ag2YczvEoXifdQsOyvT1xAPI',
+    authDomain: 'money-flow-budget-tracker.firebaseapp.com',
+    databaseURL: 'https://money-flow-budget-tracker.firebaseio.com',
+    projectId: 'money-flow-budget-tracker',
+    storageBucket: 'money-flow-budget-tracker.appspot.com',
+    messagingSenderId: '220277716810',
+    appId: '1:220277716810:web:dc3c4652e0c26ca45373b3',
+    measurementId: 'G-KD3XHV59E0'
   }
 };
